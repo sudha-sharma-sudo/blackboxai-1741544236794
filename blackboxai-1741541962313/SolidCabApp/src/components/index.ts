b39b597d16959635fa5ed5a24c4abf338eff3733
sudha-sharma-sudo <sudha.sharma2145@gmail.com>
2025-03-10 00:58:28 +0530

    Add blackboxai-1741541962313/SolidCabApp/src/components/index.ts 1741548508882

--- blackboxai-1741541962313/SolidCabApp/src/components/index.ts
+++ blackboxai-1741541962313/SolidCabApp/src/components/index.ts
@@ -1,6 +1,17 @@
-export * from './TripTypeSelector';
-export * from './LocationDropdown';
-export * from './DateTimeSelector';
-export * from './CarCard';
-export * from './BookingCard';
-export * from './FareDetailsModal';
+export { default as Button } from './Button';
+export { default as CarCard } from './CarCard';
+export { default as DateTimeSelector } from './DateTimeSelector';
+export { default as ErrorBoundary } from './ErrorBoundary';
+export { default as ErrorMessage } from './ErrorMessage';
+export { default as FareDetailsModal } from './FareDetailsModal';
+export { default as LocationDropdown } from './LocationDropdown';
+export { default as TripTypeSelector } from './TripTypeSelector';
+
+// Export types
+export type { ButtonProps } from './Button';
+export type { CarCardProps } from './CarCard';
+export type { DateTimeSelectorProps } from './DateTimeSelector';
+export type { ErrorMessageProps } from './ErrorMessage';
+export type { FareDetailsModalProps } from './FareDetailsModal';
+export type { LocationDropdownProps } from './LocationDropdown';
+export type { TripTypeSelectorProps } from './TripTypeSelector';
