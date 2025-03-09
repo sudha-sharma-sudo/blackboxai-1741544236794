2621b261384f05f2748810302d38ec73a01714ce
sudha-sharma-sudo <sudha.sharma2145@gmail.com>
2025-03-10 00:58:18 +0530

    Add blackboxai-1741541962313/SolidCabApp/src/screens/index.ts 1741548498962

--- blackboxai-1741541962313/SolidCabApp/src/screens/index.ts
+++ blackboxai-1741541962313/SolidCabApp/src/screens/index.ts
@@ -1,8 +1,18 @@
-export * from './HomeScreen';
-export * from './BookingDetailsScreen';
-export * from './ConfirmationScreen';
-export * from './ProfileScreen';
-export * from './MyRidesScreen';
-export * from './WalletScreen';
-export * from './SupportScreen';
-export * from './NotificationScreen';
+export { default as HomeScreen } from './HomeScreen';
+export { default as BookingConfirmationScreen } from './BookingConfirmationScreen';
+export { default as RideDetailsScreen } from './RideDetailsScreen';
+export { default as PaymentScreen } from './PaymentScreen';
+export { default as MyRidesScreen } from './MyRidesScreen';
+export { default as ProfileScreen } from './ProfileScreen';
+export { default as SettingsScreen } from './SettingsScreen';
+export { default as SupportScreen } from './SupportScreen';
+
+// Export types
+export type { HomeScreenProps } from './HomeScreen';
+export type { BookingConfirmationScreenProps } from './BookingConfirmationScreen';
+export type { RideDetailsScreenProps } from './RideDetailsScreen';
+export type { PaymentScreenProps } from './PaymentScreen';
+export type { MyRidesScreenProps } from './MyRidesScreen';
+export type { ProfileScreenProps } from './ProfileScreen';
+export type { SettingsScreenProps } from './SettingsScreen';
+export type { SupportScreenProps } from './SupportScreen';
