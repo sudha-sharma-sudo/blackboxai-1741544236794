999ffe3fd929d36d9c8722ba03d18eaff2c19b68
sudha-sharma-sudo <sudha.sharma2145@gmail.com>
2025-03-10 00:58:17 +0530

    Add blackboxai-1741541962313/SolidCabApp/src/types/navigation.ts 1741548497587

--- blackboxai-1741541962313/SolidCabApp/src/types/navigation.ts
+++ blackboxai-1741541962313/SolidCabApp/src/types/navigation.ts
@@ -1,24 +1,54 @@
 import { NavigatorScreenParams } from '@react-navigation/native';
+import { StackScreenProps } from '@react-navigation/stack';
+import { DrawerScreenProps } from '@react-navigation/drawer';
 
+// Main Stack Navigator Types
 export type RootStackParamList = {
-  DrawerHome: NavigatorScreenParams<DrawerParamList>;
-  BookingDetails: { bookingId: string };
-  Confirmation: { bookingId: string };
+  Main: NavigatorScreenParams<DrawerParamList>;
+  BookingConfirmation: {
+    bookingId: string;
+    pickupLocation: string;
+    dropLocation: string;
+    date: string;
+    time: string;
+    carType: string;
+    fare: number;
+  };
+  RideDetails: {
+    rideId: string;
+  };
+  Payment: {
+    amount: number;
+    bookingId: string;
+  };
 };
 
+// Drawer Navigator Types
 export type DrawerParamList = {
   Home: undefined;
   MyRides: undefined;
   Profile: undefined;
-  Wallet: undefined;
+  Settings: undefined;
   Support: undefined;
-  Notifications: undefined;
 };
+
+// Screen Props Types
+export type RootStackScreenProps<T extends keyof RootStackParamList> = StackScreenProps<
+  RootStackParamList,
+  T
+>;
+
+export type DrawerScreenProps<T extends keyof DrawerParamList> = DrawerScreenProps<
+  DrawerParamList,
+  T
+>;
+
+// Helper Types
+export type NavigationProps = RootStackScreenProps<keyof RootStackParamList>;
+export type DrawerProps = DrawerScreenProps<keyof DrawerParamList>;
 
 declare global {
   namespace ReactNavigation {
     interface RootParamList extends RootStackParamList {}
   }
 }
-
-export {};
